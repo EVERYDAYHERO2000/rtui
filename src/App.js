8ac64de8c9567fbdc4321c52a1a6aaa68fc77b62
EVERYDAYHERO2000 <ilyaflare@gmail.com>
2017-07-14 00:01:47 +0300

    Start

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
-import Less from 'less';
-import logo from './logo.svg';
-import './App.css';
+import Text from './components/text/text';
 
 class App extends Component {
   render() {
     return (
+			
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+				<Text size="m" content="Hello world!" />
+        
       </div>
     );
   }
--- src/components/text/text.js
+++ src/components/text/text.js
@@ -1,20 +1,36 @@
-define(function(require){
+import React, { Component } from 'react';
 
-  var React = require('../../libs/react/react');
+class Text extends Component {
+  render() {
+		var content = this.props.content;
+		
+		var size = (function(s){
+		var size;
+		switch (s) {
+			case 'xs':
+				size = 'text_xs';
+				break;
+			case 's':
+				size = 'text_s';
+				break;
+			case 'm':
+				size = 'text_m';
+				break;
+			case 'l':
+				size = 'text_l';
+				break;	
+			default :
+				size = '';
+				break;	
+		}
+			return size;	
+		})(this.props.size);
 
-  var App = React.createClass({
-  render: function() {
+		
     return (
-      <div className="app">
-        Всем привет, я компонент App!
-      </div>
+      <p className={['text', size].join(' ')}>{content}</p>
     );
   }
-});
-  
+}
 
-  
-
-  return App;
-
-});
+export default Text;
